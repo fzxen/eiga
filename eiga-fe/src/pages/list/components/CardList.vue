13625aa071741ab354847b75ecc70c73f718e6b7
ZXFAN <zx_fan@outlook.com>
2020-12-21 14:19:30 +0800

    feat: list page

---
+++ eiga-fe/src/pages/list/components/CardList.vue
@@ -0,0 +1,112 @@
+<template>
+  <scroll-view :scrollY="true" class="card-list">
+    <view :class="['card-wrap', type]">
+      <view class="card" v-for="c of data" :key="c.cover">
+        <view
+          class="img-container"
+          :style="{ 'background-image': `url(${c.cover})` }"
+        />
+        <text class="title">{{ c.title }}</text>
+        <text class="release">{{ c.release }}</text>
+      </view>
+    </view>
+  </scroll-view>
+</template>
+
+<script lang="ts">
+import {} from "vue";
+import {} from "@tarojs/taro";
+
+export default {
+  name: "CardList",
+
+  props: {
+    type: {
+      type: String,
+      default: "line",
+      validator: (val: string) => ["line", "grid"].indexOf(val) >= 0
+    },
+    data: {
+      required: true,
+      type: Array
+    }
+  },
+
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.card-wrap {
+  &.grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 23px;
+    .card {
+      height: 200px;
+      padding-bottom: 48px;
+
+      .title {
+        left: 5px;
+        bottom: 18px;
+
+        font-family: Montserrat-ExtraBold;
+        font-size: 14px;
+        color: #607395;
+      }
+      .release {
+        left: 5px;
+        bottom: 0;
+
+        font-family: Montserrat-SemiBold;
+        font-size: 12px;
+        color: #000000;
+        opacity: 0.54;
+      }
+    }
+  }
+  &.line {
+    .card {
+      &:not(:first-of-type) {
+        margin-top: 16px;
+      }
+    }
+  }
+  .card {
+    position: relative;
+    height: 160px;
+    overflow: hidden;
+
+    .img-container {
+      width: 100%;
+      height: 100%;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      border-radius: 10px;
+    }
+
+    .title,
+    .release {
+      position: absolute;
+      left: 20px;
+    }
+    .title {
+      bottom: 40px;
+
+      font-family: Montserrat-ExtraBold;
+      font-size: 20px;
+      color: #fefefe;
+    }
+    .release {
+      bottom: 20px;
+
+      font-family: Montserrat-SemiBold;
+      font-size: 12px;
+      color: #ffffff;
+    }
+  }
+}
+</style>
--- eiga-fe/src/pages/list/list.vue
+++ eiga-fe/src/pages/list/list.vue
@@ -1,23 +1,96 @@
 <template>
   <view class="list-wrap tab-container">
-    list
+    <Switcher class="switcher" :btns="btns" v-model="tabValue" />
+
+    <swiper class="swiper" :current="tabValue">
+      <swiper-item>
+        <CardList class="card-list" :data="cardData" />
+      </swiper-item>
+      <swiper-item>
+        <CardList class="card-list" type="grid" :data="cardData" />
+      </swiper-item>
+    </swiper>
+
     <ETabBar />
   </view>
 </template>
 
 <script lang="ts">
-import {} from "vue";
+import { ref } from "vue";
+import Switcher from "../../components/Switcher.vue";
+import CardList from "./components/CardList.vue";
 
 export default {
   name: "List",
 
+  components: { Switcher, CardList },
+
   setup() {
-    return {};
+    const btns = [
+      {
+        id: 0,
+        text: "IN THEATERS"
+      },
+      {
+        id: 1,
+        text: "COMING SOON"
+      }
+    ];
+
+    const cardData = [
+      {
+        cover: require("../../images/poster1.png"),
+        title: "12 STRONG",
+        release: "OPENING JAN 19"
+      },
+      {
+        cover: require("../../images/poster2.png"),
+        title: "12 STRONG",
+        release: "OPENING JAN 19"
+      },
+      {
+        cover: require("../../images/poster3.png"),
+        title: "12 STRONG",
+        release: "OPENING JAN 19"
+      },
+      {
+        cover: require("../../images/poster4.png"),
+        title: "12 STRONG",
+        release: "OPENING JAN 19"
+      },
+      {
+        cover: require("../../images/poster5.png"),
+        title: "12 STRONG",
+        release: "OPENING JAN 19"
+      }
+    ];
+
+    const tabValue = ref(0);
+    return { tabValue, btns, cardData };
   }
 };
 </script>
 
 <style lang="scss">
 .list-wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 16px;
+
+  .switcher {
+    margin-top: 40px;
+  }
+
+  .swiper {
+    margin-top: 31px;
+    width: 100%;
+    height: 83%;
+
+    .card-list {
+      // flex: 1;
+      height: 100%;
+    }
+  }
 }
 </style>
