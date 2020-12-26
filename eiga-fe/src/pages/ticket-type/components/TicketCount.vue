800284f125c0896059994955bbad76997fcc8239
ZXFAN <zx_fan@outlook.com>
2020-12-26 18:05:44 +0800

    feat: ticket type

---
+++ eiga-fe/src/pages/ticket-type/components/TicketCount.vue
@@ -0,0 +1,84 @@
+<template>
+  <view class="ticket-count-wrap">
+    <image
+      class="img"
+      src="~_images/minus.png"
+      @tap="$emit('update:modelValue', modelValue <= 0 ? 0 : modelValue - 1)"
+    />
+
+    <view class="text-info flex-center">
+      <text class="type">{{ modelValue }} {{ type }}</text>
+      <text class="sub">{{ sub }}</text>
+      <text class="money">$ {{ price }}</text>
+    </view>
+
+    <image
+      class="img"
+      src="~_images/plus.png"
+      @tap="$emit('update:modelValue', modelValue + 1)"
+    />
+  </view>
+</template>
+
+<script lang="ts">
+import {} from "vue";
+
+export default {
+  name: "TicketCount",
+
+  props: {
+    modelValue: {
+      required: true,
+      type: Number
+    },
+    type: {
+      required: true,
+      type: String
+    },
+    sub: String,
+    price: Number
+  },
+
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.ticket-count-wrap {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .img {
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+  }
+
+  .text-info {
+    flex: 1;
+    flex-direction: column;
+
+    .type {
+      font-family: Montserrat-ExtraBold;
+      font-size: 20px;
+      color: #607395;
+    }
+    .sub {
+      margin-top: 4px;
+      font-family: Montserrat-SemiBold;
+      font-size: 14px;
+      color: #000000;
+      opacity: 0.54;
+    }
+    .money {
+      margin-top: 6px;
+      font-family: Montserrat-SemiBold;
+      font-size: 16px;
+      color: #607395;
+    }
+  }
+}
+</style>
--- eiga-fe/src/pages/ticket-type/ticket-type.vue
+++ eiga-fe/src/pages/ticket-type/ticket-type.vue
@@ -1,20 +1,88 @@
 <template>
-  <div class="ticket-type-wrap">ticket type</div>
+  <view class="ticket-type-wrap container">
+    <view class="title flex-center">TICKET TYPE</view>
+    <view class="counts">
+      <TicketCount type="ADULT" sub="" price="14.50" v-model="adult" />
+      <TicketCount type="CHILD" sub="Age 2-12" price="11.25" v-model="child" />
+      <TicketCount type="SENIOR" sub="Age 60+" price="12.50" v-model="senior" />
+    </view>
+
+    <view class="total-price flex-center">
+      <text class="price">$ {{ totalPrice }}</text>
+      <text class="total">Total</text>
+    </view>
+
+    <EButton class="button">NEXT</EButton>
+  </view>
 </template>
 
 <script lang="ts">
-import {} from "vue";
+import { computed, ref } from "vue";
+import TicketCount from "./components/TicketCount.vue";
 
 export default {
   name: "TicketType",
 
+  components: { TicketCount },
+
   setup() {
-    return {};
+    const price = {
+      adult: 14.5,
+      child: 11.25,
+      senior: 12.5
+    };
+    const adult = ref(1);
+    const child = ref(1);
+    const senior = ref(1);
+
+    const totalPrice = computed(() => {
+      const total =
+        adult.value * price.adult +
+        child.value * price.child +
+        senior.value * price.senior;
+      return total.toFixed(2);
+    });
+    return { adult, child, senior, totalPrice };
   }
 };
 </script>
 
 <style lang="scss">
 .ticket-type-wrap {
+  .title {
+    padding-top: 19px;
+    padding-bottom: 21px;
+    font-family: Montserrat-Bold;
+    font-size: 24px;
+    color: #607395;
+    background: #ffffff;
+  }
+  .counts {
+    padding: 39px 35px 47px 35px;
+    background: #ffffff;
+    border-radius: 0 0 25px 25px;
+
+    .ticket-count-wrap:not(:first-of-type) {
+      margin-top: 39px;
+    }
+  }
+  .total-price {
+    flex-direction: column;
+    margin: 50px 0;
+    .price {
+      font-family: Montserrat-Medium;
+      font-size: 44px;
+      color: #607395;
+    }
+    .total {
+      font-family: Montserrat-SemiBold;
+      font-size: 16px;
+      color: #000000;
+      opacity: 0.54;
+    }
+  }
+  .button {
+    margin: 10px 35px;
+  }
 }
 </style>
