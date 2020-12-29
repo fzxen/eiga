6c8ed70afe503ef16f02ea758c17d70dfe09c462
ZXFAN <zx_fan@outlook.com>
2020-12-29 14:32:18 +0800

    feat: payment

---
+++ eiga-fe/src/components/Switch.vue
@@ -0,0 +1,61 @@
+<template>
+  <view
+    :class="['switch-wrap', { active: modelValue }]"
+    @tap="$emit('update:modelValue', !modelValue)"
+  >
+    <view class="track"></view>
+  </view>
+</template>
+
+<script lang="ts">
+import {} from "vue";
+
+export default {
+  name: "Switch",
+
+  props: {
+    modelValue: {
+      required: true,
+      type: Boolean
+    }
+  },
+
+  setup() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.switch-wrap {
+  display: inline-block;
+  position: relative;
+  padding: 0 5px;
+  width: 60px;
+  height: 30px;
+  background-color: #d2d8e1;
+  box-shadow: inset 0px 3px 7px 0px rgba(195, 220, 229, 0.35);
+  border-radius: 15px;
+  opacity: 0.5;
+
+  .track {
+    position: absolute;
+    top: 50%;
+    left: 5%;
+    transform: translateY(-50%);
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    background-color: #ffffff;
+    border-radius: 12px;
+    transition: all 0.2s ease-in-out;
+  }
+
+  &.active {
+    background: #5d84b4;
+    .track {
+      left: 60%;
+    }
+  }
+}
+</style>
--- eiga-fe/src/pages/pay/pay.vue
+++ eiga-fe/src/pages/pay/pay.vue
@@ -1,31 +1,110 @@
 <template>
-  <view class="pay-wrap">
-
+  <view class="pay-wrap container">
     <view class="cover">
-      <image src="~_images/poster1.png"/>
+      <image src="~_images/poster1.png" />
     </view>
 
-    <view class="info"></view>
+    <view class="info">
+      <text class="normal">Reald 3D, 10 dec4:35pm</text>
+      <text class="normal">Row 8, seats 5,6,7</text>
+      <text class="exclaim">Total: $ 24</text>
+    </view>
 
-    <view class="promocode"></view>
+    <view class="promocode">
+      <text>Use promocode?</text> <ESwitch v-model="usePromo" />
+    </view>
 
-    <view class="bankcard"></view>
+    <view class="bankcard">
+      <swiper class="swiper" previousMargin="100rpx" nextMargin="100rpx">
+        <swiper-item class="swiper-item"><Bankcard /></swiper-item>
+        <swiper-item class="swiper-item"><Bankcard /></swiper-item>
+        <swiper-item class="swiper-item"><Bankcard /></swiper-item>
+      </swiper>
+    </view>
+
+    <view class="btn">
+      <EButton>Traditional Payment</EButton>
+    </view>
   </view>
 </template>
 
 <script lang="ts">
-import {} from "vue";
+import ESwitch from "../../components/Switch.vue";
+import Bankcard from "../../components/Bankcard.vue";
+
+import { ref } from "vue";
 
 export default {
   name: "Pay",
 
+  components: { ESwitch, Bankcard },
+
   setup() {
-    return {};
+    const usePromo = ref(false);
+    return { usePromo };
   }
 };
 </script>
 
 <style lang="scss">
 .pay-wrap {
+  .cover {
+    margin-top: 32px;
+    padding: 0 17px;
+    border-radius: 10px;
+    height: 160px;
+    overflow: hidden;
+    text-align: center;
+    filter: drop-shadow(0, 1px, 3px, crimson);
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .info {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+    padding: 0 34px;
+    text {
+      font-family: Montserrat-SemiBold;
+      color: #607395;
+    }
+    .normal {
+      margin-top: 11px;
+      font-size: 16px;
+    }
+    .exclaim {
+      margin-top: 25px;
+      font-size: 22px;
+    }
+  }
+  .promocode {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 29px;
+    padding: 0 34px;
+    text {
+      font-family: Montserrat-SemiBold;
+      font-size: 16px;
+      color: #607395;
+    }
+  }
+  .bankcard {
+    margin-top: 35px;
+    padding: 0;
+    .swiper {
+      height: 160px;
+
+      &-item {
+        width: 280px !important;
+      }
+    }
+  }
+  .btn {
+    margin-top: 47px;
+    padding: 0 34px;
+  }
 }
 </style>
